ddce3beacc2440e562fc37308b7d2564f1050557
Luan Carvalho <luanjcarvalho@gmail.com>
2022-10-28 12:09:29 -0300

    Inicializando projeto com scaffold

---
+++ teste-suno-by-luan-carvalho.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Teste Suno By Luan Carvalho
+ * Plugin URI:      https://github.com/LuanJCarvalho/
+ * Description:     Mostra dados de um ativo da Nasdaq
+ * Author:          Luan Jacinto Carvalho
+ * Author URI:      https://www.linkedin.com/in/luanjcarvalho/
+ * Text Domain:     teste-suno-by-luan-carvalho
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Teste_Suno_By_Luan_Carvalho
+ */
+
+// Your code starts here.
